78219d87fca89426540eede037de4d9bed0005a9
Elliot Nash <elliotnash@gmail.com>
2023-12-03 12:27:17 -0800

    Day 3 part 2 complete

--- day3/main.c
+++ day3/main.c
@@ -15,8 +15,42 @@ int atoin(const char* start, const char* end) {
     return out;
 }
 
+typedef struct {
+    int line;
+    int start;
+    int end;
+    int value;
+} part_t;
+
+part_t part_from_match(char** lines, int i, int j) {
+    // Find end
+    int end = j;
+    while (isnumber(lines[i][++end])) {}
+    --end;
+    // Find start
+    int start = j;
+    while (isnumber(lines[i][--start])) {}
+    ++start;
+    // Get value
+    const int value = atoin(lines[i]+start, lines[i]+end);
+    return (part_t) {i, start, end, value};
+}
+
+bool in_part(const part_t* part, const int y, const int x) {
+    return y == part->line && x >= part->start && x <= part->end;
+}
+
+bool in_parts(const part_t* parts, const int length, const int y, const int x) {
+    for (int i = 0; i < length; ++i) {
+        if (in_part(parts+i, y, x)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
-    FILE* fp = fopen("input.txt", "r");;
+    FILE* fp = fopen("input.txt", "r");
 
     if (fp == NULL) {
         printf("Failed to open input.txt! (did you set the working directory?)\n");
@@ -30,7 +64,7 @@ int main() {
     size_t num_lines = 0;
 
     size_t len = 0;
-    ssize_t line_len = 0;
+    ssize_t line_len;
     while ((line_len = getline(lines + num_lines, &len, fp)) != -1) {
         if (lines[num_lines][line_len-1] == '\n') {
             lines[num_lines][line_len-1] = '\0';
@@ -88,6 +122,39 @@ int main() {
                     }
                 } else {
                     ++j;
+                }
+            }
+        }
+    }
+
+    // Part 2
+    {
+        for (int i = 0; i < num_lines; ++i) {
+            for (int j = 0; lines[i][j] != '\0'; ++j) {
+                if (lines[i][j] == '*') {
+                    // We've found a potential gear ratio to search
+                    part_t* parts = malloc(sizeof(part_t) * 8);
+                    int num_parts = 0;
+                    // Loop through all adjecent positions.
+                    for (int y = i-1; y <= i+1; ++y) {
+                        for (int x = j-1; x <= j+1; ++x) {
+                            // Make sure position is valid
+                            if (y >= 0 && x >= 0 && y < num_lines && isnumber(lines[y][x])) {
+                                // If this is a new adjecent number, add it
+                                if (!in_parts(parts, num_parts, y, x)) {
+                                    parts[num_parts++] = part_from_match(lines, y, x);
+                                }
+                            } else {
+                            }
+                        }
+                    }
+
+                    if (num_parts == 2) {
+                        // If we have exactly 2 adjecent parts, add the gear ratio
+                        part2sum += parts[0].value * parts[1].value;
+                    }
+
+                    free(parts);
                 }
             }
         }
